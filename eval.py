56fb3366863200e1f48dfe4e3acad47ce5246baa
Tramac <1145955920@qq.com>
2019-03-19 19:51:05 +0800

    update eval.py

--- eval.py
+++ eval.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 
 import os
 import argparse
-import numpy as np
 
 import torch
 import torch.utils.data as data
@@ -11,11 +10,7 @@ from data_loader.transforms import TestTransform
 from data_loader.voc import VOCSegmentation
 from models.model_zoo import get_model
 from utils.score import SegmentationMetric
-
-from utils.score import hist_info, compute_score
-from utils.visualize import print_iou, save_colorful_images
-from data_loader.voc import VOC_PALETTE, VOC_CLASSES, MEAN, STD
-from data_loader.functional import denomalize, to_pil_image
+from utils.visualize import get_color_pallete
 
 parser = argparse.ArgumentParser(
     description='Semantic Segmentation Evaluation')
@@ -29,26 +24,19 @@ parser.add_argument('--dataset', default='VOC2012', choices=['VOC2007', 'VOC2012
                     type=str, help='VOC2007 or VOC2012')
 parser.add_argument('--num_classes', default=21, type=int,
                     help='Number of classes.')
+parser.add_argument('--save-result', default=False,
+                    help='save the predict')
 parser.add_argument('--outdir', default='./eval', type=str,
                     help='path to save the predict result')
 args = parser.parse_args()
 
 
-def fast_hist(pred, label, n):
-    k = (label >= 0) & (label < n)
-    return np.bincount(
-        n * label[k].astype(int) + pred[k], minlength=n ** 2).reshape(n, n)
-
-
-def per_class_iu(hist):
-    return np.diag(hist) / (hist.sum(1) + hist.sum(0) - np.diag(hist))
-
-
 def eval(config):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # output folder
-    if not os.path.exists(config.outdir):
-        os.makedirs(config.outdir)
+    if config.save_result:
+        if not os.path.exists(config.outdir):
+            os.makedirs(config.outdir)
     # image transform
     input_transform = TestTransform([.485, .456, .406], [.229, .224, .225])
 
@@ -71,31 +59,25 @@ def eval(config):
     metric = SegmentationMetric(config.num_classes)
 
     model.eval()
-    hist = np.zeros((config.num_classes, config.num_classes))
-    correct = 0
-    labeled = 0
     for i, (image, label) in enumerate(test_loader):
         image = image.to(device)
 
         outputs = model(image)
 
-        _, pred = torch.max(outputs, 1)
+        pred = torch.argmax(outputs, 1)
         pred = pred.data.numpy()
-
         label = label.numpy()
+
         metric.update(pred, label)
         pixAcc, mIoU = metric.get()
-        print('mIoU: %.8f, pixAcc: %.8f' % (mIoU * 100, pixAcc * 100))
-        hist_tmp, labeled_tmp, correct_tmp = hist_info(pred.flatten(), label.flatten(), config.num_classes)
-        hist += hist_tmp
-        labeled += labeled_tmp
-        correct += correct_tmp
-        iu, mean_IU, _, mean_pixel_acc = compute_score(hist, correct, labeled)
-        print_iou(iu, mean_pixel_acc, VOC_CLASSES, False)
-        # img = to_pil_image(denomalize(image.squeeze(), MEAN, STD))
-        # img.save('./eval/img_%d.png' % i)
-        # save_colorful_images(pred, 'test_%d.png' % i, './eval', VOC_PALETTE)
+        print('pixAcc: %.3f%%, mIoU: %.3f%%' % (pixAcc * 100, mIoU * 100))
+
+        if config.save_result:
+            predict = pred.squeeze(0)
+            mask = get_color_pallete(predict, config.dataset)
+            mask.save(os.path.join(config.outdir, 'seg_' + str(i) + '_.png'))
 
 
 if __name__ == '__main__':
+    print('Testing model: ', args.model)
     eval(args)
