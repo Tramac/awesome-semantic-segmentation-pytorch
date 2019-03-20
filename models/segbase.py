f8f07e2f56351b16630a39d1307b7a99efa88e09
Tramac <1145955920@qq.com>
2019-03-20 16:40:08 +0800

    add basemodel resnet,segbase

---
+++ models/segbase.py
@@ -0,0 +1,66 @@
+"""Base Model for Semantic Segmentation"""
+import torch.nn as nn
+from .resnet import resnet50, resnet101, resnet152
+
+
+class SegBaseModel(nn.Module):
+    r"""Base Model for Semantic Segmentation
+
+    Parameters
+    ----------
+    backbone : string
+        Pre-trained dilated backbone network type (default:'resnet50'; 'resnet50',
+        'resnet101' or 'resnet152').
+    """
+
+    def __init__(self, nclass, aux, backbone='resnet50', height=None, width=None,
+                 base_size=520, crop_size=480, pretrained_base=True, **kwargs):
+        super(SegBaseModel, self).__init__()
+        self.aux = aux
+        self.nclass = nclass
+        if backbone == 'resnet50':
+            pretrained = resnet50(pretrained=pretrained_base, **kwargs)
+        elif backbone == 'resnet101':
+            pretrained = resnet101(pretrained=pretrained_base, **kwargs)
+        elif backbone == 'resnet152':
+            pretrained = resnet152(pretrained=pretrained_base, **kwargs)
+        else:
+            raise RuntimeError('unknown backbone: {}'.format(backbone))
+        self.conv1 = pretrained.conv1
+        self.bn1 = pretrained.bn1
+        self.relu = pretrained.relu
+        self.maxpool = pretrained.maxpool
+        self.layer1 = pretrained.layer1
+        self.layer2 = pretrained.layer2
+        self.layer3 = pretrained.layer3
+        self.layer4 = pretrained.layer4
+        height = height if height is not None else crop_size
+        width = width if width is not None else crop_size
+        self._up_kwargs = {'height': height, 'width': width}
+        self.base_size = base_size
+        self.crop_size = crop_size
+
+    def base_forward(self, x):
+        """forwarding pre-trained network"""
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        c3 = self.layer3(x)
+        c4 = self.layer4(c3)
+        return c3, c4
+
+    def evaluate(self, x):
+        """evaluating network with inputs and targets"""
+        return self.forward(x)[0]
+
+    def demo(self, x):
+        h, w = x.shape[2:]
+        self._up_kwargs['height'] = h
+        self._up_kwargs['width'] = w
+        pred = self.forward(x)
+        if self.aux:
+            pred = pred[0]
+        return pred
