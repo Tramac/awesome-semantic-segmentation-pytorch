2ba9df69597cd42c730514ef7e105a7771f10056
Tramac <1145955920@qq.com>
2019-06-17 19:40:49 +0800

    fix bug

--- core/utils/lr_scheduler.py
+++ core/utils/lr_scheduler.py
@@ -151,21 +151,23 @@ class WarmupPolyLR(torch.optim.lr_scheduler._LRScheduler):
         self.warmup_factor = warmup_factor
         self.warmup_iters = warmup_iters
         self.warmup_method = warmup_method
+
         super(WarmupPolyLR, self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
         N = self.max_iters - self.warmup_iters
         T = self.last_epoch - self.warmup_iters
-        warmup_factor = 1
         if self.last_epoch < self.warmup_iters:
             if self.warmup_method == 'constant':
                 warmup_factor = self.warmup_factor
-            elif self.warmup_factor == 'linear':
+            elif self.warmup_method == 'linear':
                 alpha = float(self.last_epoch) / self.warmup_iters
                 warmup_factor = self.warmup_factor * (1 - alpha) + alpha
+            else:
+                raise ValueError("Unknown warmup type.")
+            return [self.target_lr + (base_lr - self.target_lr) * warmup_factor for base_lr in self.base_lrs]
         factor = pow(1 - T / N, self.power)
-        return [self.target_lr + (base_lr - self.target_lr) * factor * warmup_factor
-                for base_lr in self.base_lrs]
+        return [self.target_lr + (base_lr - self.target_lr) * factor for base_lr in self.base_lrs]
 
 
 if __name__ == '__main__':
