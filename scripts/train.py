fa4c9888376bb78f2d3301fe2719552f89e26a01
FUTUREEEEEE <52389798+FUTUREEEEEE@users.noreply.github.com>
2021-03-10 20:47:32 +0800

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -26,8 +26,8 @@ from core.utils.score import SegmentationMetric
 
 from torch.utils.tensorboard import SummaryWriter
 # 定义该次实验名称
-tang_log_dir=input()
-writer = SummaryWriter(tang_log_dir)
+
+writer = SummaryWriter("/content/drive/MyDrive/deeplabv3/log/resnet152/tang1")
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Semantic Segmentation Training With Pytorch')
