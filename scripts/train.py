768a6aa0368fb6d55507dbd3ba2934243dec77e3
Jeralyn123 <43395674+Jeralyn123@users.noreply.github.com>
2022-03-21 10:09:43 +0800

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -28,14 +28,11 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Semantic Segmentation Training With Pytorch')
     # model and dataset
     parser.add_argument('--model', type=str, default='fcn',
-                        choices=['swnet','fcn32s', 'fcn16s', 'fcn8s', 'fcn', 'psp', 'deeplabv3', 
-                            'deeplabv3_plus', 'danet', 'denseaspp', 'bisenet', 'encnet', 
-                            'dunet', 'icnet', 'enet', 'ocnet', 'psanet', 'cgnet', 'espnet', 
-                            'lednet', 'dfanet'],
+                        choices=['swnet'],
                         help='model name (default: fcn32s)')
     parser.add_argument('--backbone', type=str, default='resnet50',
-                        choices=['vgg16', 'resnet18', 'resnet50', 'resnet101', 'resnet152', 
-                            'densenet121', 'densenet161', 'densenet169', 'densenet201'],
+                        choices=['resnet50', 'resnet101', 'resnet152', 
+                            ],
                         help='backbone name (default: vgg16)')
     parser.add_argument('--dataset', type=str, default='pascal_voc',
                         choices=['pascal_voc', 'pascal_aug', 'ade20k', 'citys', 'sbu'],
